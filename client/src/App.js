4567a20d95791b1b2397aca38280955223cb31ce
sbecenti <samsonbecenti@gmail.com>
2024-01-31 21:41:36 -0700

    Testing components like Map and Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 // import './App.css';
 // import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import Map from './components/Map.js';
+//import NavBar from './components/NavBar.js';
+
 
 function App() {
   return (
-   <>
+
+    <section>
+      <Map />
+    </section>
+
+    
+   /*<>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1698862.63706548!2d-113.02528221328376!3d33.73246065571182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b73b0942b05eb%3A0xc4ec9b1c5630b985!2sREI!5e0!3m2!1sen!2sus!4v1705522979273!5m2!1sen!2sus" 
     width="600" 
     height="450" 
@@ -14,7 +23,7 @@ function App() {
     title="Google Maps"
     ></iframe>
    
-   </>
+   </>*/
   );
 }
 
